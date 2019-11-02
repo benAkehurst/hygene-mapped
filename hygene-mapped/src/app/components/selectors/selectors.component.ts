5bc85908d2de6c952db7a41226bed490bd03a139
benakehurst <benakehurst@hotmail.com>
2019-11-02 12:29:13 +0000

    feature(selector): use selector component

--- hygene-mapped/src/app/components/selectors/selectors.component.ts
+++ hygene-mapped/src/app/components/selectors/selectors.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { DataService } from './../../../services/data.service';
+import { Place } from '../../../models/place.model';
+import { londonData, ratings } from '../../../data/london.factory';
 
 @Component({
   selector: 'app-selectors',
@@ -6,10 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./selectors.component.scss']
 })
 export class SelectorsComponent implements OnInit {
+  public isLoading = false;
+  public isReceived = false;
+  public authorities: any;
+  public ratings: Array<number>;
+  public place: Place;
+  public loadedPlaces: Place[];
+  public chosenRating: string;
+  public chosenAuthority: string;
 
-  constructor() { }
+  constructor(private data: DataService) {}
 
   ngOnInit() {
+    this.authorities = londonData;
+    this.ratings = ratings;
   }
 
+  public selectAuthority(auth) {
+    this.chosenAuthority = auth;
+  }
+
+  public selectRating(rating) {
+    this.chosenRating = rating.toString();
+  }
+
+  public selectPlaceParams() {
+    this.getSelectedPlace(this.chosenRating, this.chosenAuthority);
+  }
+
+  public getSelectedPlace(rating: string, authority: string) {
+    this.isLoading = true;
+    this.data.getLocationData(rating, authority).subscribe(res => {
+      this.loadedPlaces = res;
+      if (this.loadedPlaces.length > 1) {
+        this.data.selectedLocations(this.loadedPlaces);
+      }
+      this.isLoading = false;
+      this.isReceived = true;
+    });
+  }
 }
