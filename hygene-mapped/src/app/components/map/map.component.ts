6356d21febfe688c5aaad56210fc77251efc077d
benakehurst <benakehurst@hotmail.com>
2019-11-02 12:29:25 +0000

    feat(map): add map component

--- hygene-mapped/src/app/components/map/map.component.ts
+++ hygene-mapped/src/app/components/map/map.component.ts
@@ -10,38 +10,18 @@ import { londonData, ratings } from '../../../data/london.factory';
 })
 export class MapComponent implements OnInit {
   public isLoading = false;
-  public authorities: any;
-  public ratings: Array<number>;
-  public place: Place;
-  public loadedPlaces: Place[];
-  public chosenRating: string;
-  public chosenAuthority: string;
+  public lat = 51.642102;
+  public lng = -0.167059;
+  public defZoom = 9;
+  public markers: Array<any> = [];
 
   constructor(private data: DataService) {}
 
   ngOnInit() {
-    this.authorities = londonData;
-    this.ratings = ratings;
   }
 
-  public selectAuthority(auth) {
-    this.chosenAuthority = auth;
-  }
-
-  public selectRating(rating) {
-    this.chosenRating = rating.toString();
-  }
-
-  public selectPlaceParams() {
-    this.getSelectedPlace(this.chosenRating, this.chosenAuthority);
-  }
-
-  public getSelectedPlace(rating: string, authority: string) {
-    this.isLoading = true;
-    this.data.getLocationData(rating, authority).subscribe(res => {
-      this.loadedPlaces = res;
-      console.log(this.loadedPlaces);
-      this.isLoading = false;
-    });
+  public generateMap() {
+    this.markers = this.data.selectedOptions;
+    console.log(this.markers);
   }
 }
