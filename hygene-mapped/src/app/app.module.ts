d1115285cf904eea01c688c8f58fc827dc4a1f67
benakehurst <benakehurst@hotmail.com>
2019-11-02 09:24:44 +0000

    chore(app-module): add imports

--- hygene-mapped/src/app/app.module.ts
+++ hygene-mapped/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +17,13 @@ import { HeaderComponent } from './components/header/header.component';
     SelectorsComponent,
     HeaderComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
