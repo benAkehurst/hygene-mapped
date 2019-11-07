b9f62b56739579ee539c16dec2c06695c9ed909a
benakehurst <benakehurst@hotmail.com>
2019-11-07 22:34:28 +0000

    feat(interfaces): update model to interface

---
+++ src/interfaces/place.interface.ts
@@ -0,0 +1,11 @@
+export class IPlace {
+  name: string;
+  type: string;
+  address: string;
+  postCode: string;
+  phone: string;
+  ratingValue: string;
+  score: string;
+  geocode: object;
+  link: Array<object>;
+}
